9f4afe838bb94f707e85467203f1ecf13df71a64
ahmedhawas <ahmedhawas7@gmail.com>
2014-02-22 20:55:07 -0500

    add marker to map on click on the updates and cosmetic changes to map+updates

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -7,8 +7,31 @@ angular.module('sizzleAngularApp')
                 latitude: 43.7000,
                 longitude: -79.4000
             },
-            zoom: 10
+            zoom: 10,
+            markers:[
+                {
+                    latitude: 43.7000,
+                    longitude: -79.4000
+                }
+            ]
         };
         var UpdateRef = new Firebase("https://sizzleapp.firebaseio.com/updates");
         $scope.updates = $firebase(UpdateRef);
+
+        $scope.showOnMap = function(lat, long){
+            $scope.map = {
+                center:{
+                    latitude:lat,
+                    longitude: long
+                },
+                zoom: 14,
+
+                markers: [
+                    {
+                        latitude: lat,
+                        longitude: long
+                    }
+                ]
+            }
+        };
   });
